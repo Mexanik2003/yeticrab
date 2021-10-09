12cc4c420c9480795a3babc5c504350df759b68d
AD <ad@it-eburg.ru>
2021-10-09 15:26:23 +0500

    init: front

---
+++ front/components/App/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+          <nav>
+              <button>Режим администратора</button>
+          </nav>
+      </header>
+
+
+    </div>
+  );
+}
+
+export default App;
